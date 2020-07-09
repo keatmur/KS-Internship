d76bead432b10a83963e3beed37455cd12e6c193
keatmur <36660634+keatmur@users.noreply.github.com>
2020-07-09 11:24:14 +0300

    Add activity_for_result ,update activity_main.xml

--- app/src/main/java/com/example/ks_internship/activity/ForResult.java
+++ app/src/main/java/com/example/ks_internship/activity/ForResult.java
@@ -1,14 +1,17 @@
-package com.example.ks_internship;
+package com.example.ks_internship.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ks_internship.R;
+
+public class ForResult extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_for_result);
     }
 }
---
+++ app/src/main/java/com/example/ks_internship/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ks_internship.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ks_internship.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
